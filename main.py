30567124479a215cd749e3b850b8beb173fb58a2
Roman <roman.hanselmann@stud.fhgr.ch>
2023-10-02 14:53:41 +0200

    Added authorship information.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+__author__ = "Roman Hanselmann"
+#__copyright__ = "-
+#__credits__ = ["-"]
+#__license__ = "-"
+#__version__ = "1.0.1"
+__maintainer__ = "Roman Hanselmann"
+__email__ = "roman.hanselmann@stud.fhgr.ch"
+__status__ = "Development"
+
+
 #Get started
 print('Hello, World!')
