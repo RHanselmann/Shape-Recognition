d3beb461359fe544676948847dc84dd5a15bdc03
Roman <roman.hanselmann@stud.fhgr.ch>
2023-10-02 15:31:17 +0200

    Open the sample image and show it.

--- main.py
+++ main.py
@@ -8,5 +8,27 @@ __email__ = "roman.hanselmann@stud.fhgr.ch"
 __status__ = "Development"
 
 
-#Get started
-print('Hello, World!')
+# Imports
+import cv2
+  
+
+# Path to image
+path = r'D:\Unterlagen Studium\Python\Shape-Recognition\Shape-Recognition\sample_image.jpg'
+  
+# Reading an image in default mode
+image = cv2.imread(path)
+  
+# Window name in which image is displayed
+window_name = 'image'
+  
+# Using cv2.imshow() method
+# Displaying the image
+cv2.imshow(window_name, image)
+  
+# waits for user to press any key
+# (this is necessary to avoid Python kernel form crashing)
+cv2.waitKey(0)
+  
+# closing all open windows
+cv2.destroyAllWindows()
+
